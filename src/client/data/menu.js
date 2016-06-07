69c8ba33ee938bae98ac626c8de6e612ff7014d1
moosenado <egrabish@gmail.com>
2016-06-07 08:22:08 -0400

    update styles and menu comments

--- src/client/data/menu.js
+++ src/client/data/menu.js
@@ -1,60 +1,57 @@
+/*
+* RUDY MENU
+* When adding/removing items, please remember to keep the same structure that currently exists
+* Don't forget to add commas in the right places
+* It is suggested that you make a copy of this file somewhere for future reference if anything goes wrong while editing
+*/
+
 var rudy_menu = [
   {
-    id: 1,
     name: "The Rudy",
     price: "6.49",
     description: "Cheeseburger, Lettuce, Tomato & Rudy Sauce"
   },
   {
-    id: 2,
     name: "Rude Dude",
     price: "8.99",
     description: "Double Cheeseburger, Lettuce, Tomato & Rudy Sauce"
   },
   {
-    id: 3,
     name: "Valentino",
     price: "8.99",
     description: "Fried Chicken Sandwich"
   },
   {
-    id: 4,
     name: "Valenrudy",
     price: "12.99",
     description: "Rudy & Valentino Sandwich"
   },
   {
-    id: 5,
     name: "Tattooed",
     price: "1.49",
     description: "With Bacon"
   },
   {
-    id: 6,
     name: "Fritters",
     price: "3.99",
     description: ""
   },
   {
-    id: 7,
     name: "Poutine",
     price: "6.99",
     description: "Fritters, Quebecois Gravy & Cheese Curds"
   },
   {
-    id: 8,
     name: "Drinks",
     price: "1.99",
     description: ""
   },
   {
-    id: 9,
     name: "Weezie's Fudge",
     price: "1.49",
     description: ""
   },
   {
-    id: 10,
     name: "Toppings",
     price: "",
     description: "Lettuce, Tomatoes, Pickles, Onions, Ketchup, Mustard, Mayonnaise"
