32fb927d3953dfefc4b9b2b97c1a072a1b664578
moosenado <egrabish@gmail.com>
2016-06-06 20:25:48 -0400

    edit menu js

--- src/client/data/menu.js
+++ src/client/data/menu.js
@@ -1,14 +1,20 @@
 var rudy_menu = [
   {
-  	id: 1,
-  	name: "The Burger",
-  	price: "$5.99",
-  	description: "this is the description"
+    id: 1,
+    name: "The Burger",
+    price: "$5.99",
+    description: "this is the description"
   },
   {
-  	id: 2,
-  	name: "The Burger",
-  	price: "$5.99",
-  	description: "this is the description"
+    id: 2,
+    name: "The Burger",
+    price: "$5.99",
+    description: "this is the description"
+  },
+  {
+    id: 2,
+    name: "The Burger Cheeseburger",
+    price: "$25.99",
+    description: "this is the description, this is the description"
   }
 ];
