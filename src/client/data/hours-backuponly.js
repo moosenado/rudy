e3b604d0107c5a8e280c35342a5ddee7614de12a
moosenado <egrabish@gmail.com>
2016-07-30 10:56:49 -0400

    add store hours

---
+++ src/client/data/hours-backuponly.js
@@ -0,0 +1,38 @@
+/*
+* OFFICIAL RUDY HOURS - CHANGING THIS FILE WILL CHANGE THE HOURS ON THE WEBSITE
+*
+* When adding/removing items, please remember to keep the same structure that currently exists
+* Don't forget to add commas in the right places
+* It is suggested that you make a copy of this file somewhere for future reference if anything goes wrong while editing
+*/
+
+var rudy_hours = [
+  {
+    Day: "Monday",
+    Hours: "Closed"
+  },
+  {
+    Day: "Tuesday",
+    Hours: "12pm - 10pm"
+  },
+  {
+    Day: "Wednesday",
+    Hours: "12pm - 10pm"
+  },
+  {
+    Day: "Thursday",
+    Hours: "12pm - 10pm"
+  },
+  {
+    Day: "Friday",
+    Hours: "12pm - 10pm"
+  },
+  {
+    Day: "Saturday",
+    Hours: "12pm - 10pm"
+  },
+  {
+    Day: "Sunday",
+    Hours: "12pm - 8pm"
+  }
+];
